4171fb1ae0da160d52d1678fd58a7ea5c4b55718
Tomo Soch <tomosoch@gmail.com>
2024-01-10 15:06:16 +0000

    FC
    First Commit

---
+++ app/users/page.tsx
@@ -0,0 +1,11 @@
+const UsersList = () => {
+    return (
+        <div>
+            <h1>User name</h1>
+            <div>
+                <p>User details</p>
+            </div>
+        </div>);
+};
+
+export default UsersList;
